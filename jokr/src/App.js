9a89bac3887d696246f589295ac5a06d88a35487
erik-sandoval <erik.sandoval@icloud.com>
2019-05-21 11:12:14 -0700

    added axios with auth and set up private routes

--- jokr/src/App.js
+++ jokr/src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import "./App.css";
 
-import { Route } from "react-router-dom";
+import { Route, Link } from "react-router-dom";
 
 import Login from "./components/Login";
 import Register from "./components/Register";
@@ -9,6 +9,8 @@ import Register from "./components/Register";
 function App() {
   return (
     <div className="App">
+      <Link to="/login">Login</Link>
+      <Link to="/register">Register</Link>
       <Route exact path="/login" component={Login} />
       <Route path="/register" component={Register} />
     </div>
